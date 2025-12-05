29ad68d5038de9032e7d6bfbff13a92ad6cfe6c8
Harsha Vardhan Lanka <hv772005@gmail.com>
2025-12-05 19:36:24 +0530

    Remove all files from repository

--- state.py
+++
@@ -1,7 +0,0 @@
-from typing import TypedDict, List
-
-class AgentState(TypedDict):
-    topic: str                # The User's input
-    research_plan: List[str]  # Output from Planner
-    search_results: str       # Output from Searcher
-    final_report: str         # Output from Writer
